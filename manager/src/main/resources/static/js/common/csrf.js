80148f5b690be55da93dafd8915eb4f8ad5f42ad
wuruohui <591330415@qq.com>
2020-03-15 22:06:37 +0800

    [feat]manager--增加meta头部csrf令牌

---
+++ manager/src/main/resources/static/js/common/csrf.js
@@ -0,0 +1,12 @@
+layui.use(['layer'],function() {
+    var
+        $ = layui.jquery;
+    //ajax请求时都带上csrf信息
+    $(function () {
+        var token = $("meta[name='_csrf']").attr("content")
+        var header = $("meta[name='_csrf_header']").attr("content")
+        $(document).ajaxSend(function (e, xhr, options) {
+            xhr.setRequestHeader(header, token)
+        })
+    })
+})
